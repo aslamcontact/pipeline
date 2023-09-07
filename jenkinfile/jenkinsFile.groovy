fac3c46f7e9838bfc3dd68618d2820a7113d1c9c
aslamcontact <aslam.mycontact@gmail.com>
2023-09-08 01:55:44 +0530

    testing

--- jenkinfile/jenkinsFile.groovy
+++ jenkinfile/jenkinsFile.groovy
@@ -4,6 +4,7 @@ pipeline
                       docker {
                                 image 'node:18.17.1-alpine3.18'
                                 args ' -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker'
+                                reuseNode true
                             }
                     }
 
