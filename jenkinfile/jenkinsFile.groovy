49496de8d6201222a33b99c9e355d83c162b3690
aslamcontact <aslam.mycontact@gmail.com>
2023-09-08 01:32:03 +0530

    testing

--- jenkinfile/jenkinsFile.groovy
+++ jenkinfile/jenkinsFile.groovy
@@ -6,37 +6,33 @@ pipeline
                                 args ' -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker'
                             }
                     }
-            tools{
-                maven 'maven'
-            }
+
             options {
                 skipDefaultCheckout()
             }
-            environment{
-                         myname='aslam'
-                         con=false
-                       }
+
      
             stages{
 
                  stage('test1')
                          {
 
-
                             steps {
 
-                         
-
-                                        sh 'mvn -v'
-
-                                
+                                        sh 'ls'
 
                                    }
                          }
+                stage('test2')
+                        {
 
+                            steps {
 
+                                sh 'ls'
+
+                            }
+                        }
 
                  }
-
 
         }
