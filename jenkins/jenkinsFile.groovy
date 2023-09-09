f23193d5f5ef7ec9f1bc2055ac1cbdf4e5e8535e
aslamcontact <aslam.mycontact@gmail.com>
2023-09-10 01:53:49 +0530

    change bash to sh

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -3,7 +3,7 @@ pipeline
             agent {
                       docker {
                                 image 'alpine/git:latest'
-                                args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker  --entrypoint sh '
+                                args '-it --entrypoint /bin/sh -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker '
                                 reuseNode true
                             }
                     }
