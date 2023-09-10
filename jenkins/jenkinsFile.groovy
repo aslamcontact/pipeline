e9944960b49df075ef0f716cec1482a551015025
aslamcontact <aslam.mycontact@gmail.com>
2023-09-10 14:42:53 +0530

    some test

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -3,7 +3,7 @@ pipeline
             agent {
                       docker {
                                 image 'aslamimages/alpine-git:2'
-                                args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker -w /git'
+                                args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker -v ${PWD}/git -w /git'
                                 reuseNode true
                             }
                     }
