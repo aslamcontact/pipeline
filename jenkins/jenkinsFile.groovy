d8f80b17c06f5913356cbadb9116ff029622c595
aslamcontact <aslam.mycontact@gmail.com>
2023-09-10 15:18:52 +0530

    some test

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -23,7 +23,7 @@ pipeline
 
 
                                         sh 'pwd'
-                                        sh 'ls'
+                                        sh 'mvn --version'
 
 
                                    }
