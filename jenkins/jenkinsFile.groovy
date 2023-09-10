9a34410afe1c20b93b1b194edb89ddf91de9c169
aslamcontact <aslam.mycontact@gmail.com>
2023-09-11 01:02:25 +0530

    some test

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -4,6 +4,7 @@ pipeline
                 docker {
                     image 'ubuntu:latest'
                     args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker'
+                    args '-v /home/buildImage/:/buildImage'
                 }
             }
 
@@ -53,7 +54,7 @@ pipeline
                 stage('copy')
                         {
                             steps {
-                                sh "docker run --rm -v ${volume}:/app -v /home/buildImage/:/api -w /app/ci_api_test/target --name sys2 alpine:latest cp ci_test_api-0.0.1-SNAPSHOT.jar /api/product.jar"
+                                sh "docker run --rm -v ${volume}:/app  -w /app/ci_api_test/target --name sys2 alpine:latest cp ci_test_api-0.0.1-SNAPSHOT.jar /buildImage/product.jar"
                             }
                         }
                 stage('build')
@@ -62,7 +63,8 @@ pipeline
                                           buildCmd="docker build -t jdk_test:${BUILD_NUMBER} ."
                                        }
                             steps {
-                                sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker -v /home/buildImage/:/api -w /api --name sys3 ubuntu:latest ${buildCmd}"
+                                sh 'cd /buildImage'
+                                sh "${buildCmd}"
                             }
                         }
 
