355bf4d18ffc803893937e3ef5e4d4575fc81d55
aslamcontact <aslam.mycontact@gmail.com>
2023-09-10 15:11:01 +0530

    some test

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -1,11 +1,6 @@
 pipeline
         {
-            agent {
-                docker {
-                    image 'aslamimages/mvn_jdk_git:latest'
-                    args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker'
-                }
-                }
+            agent none
 
             options {
                 skipDefaultCheckout()
@@ -18,8 +13,7 @@ pipeline
                          {
                              agent {
                                  docker { image 'aslamimages/alpine-git:2'
-                                     args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker\' -v ${pwd}/git -w /git'
-                                 }
+                                     }
                              }
 
                             steps {
@@ -35,7 +29,7 @@ pipeline
                         {
                             agent {
                                 docker { image 'aslamimages/alpine-git:2'
-                                    args '-v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker\' -v ${pwd}/git -w /git'
+
                                 }
                             }
 
